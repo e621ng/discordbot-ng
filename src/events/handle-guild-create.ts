01b8d7b933732f820799b9b21495945bc3ce2855
Tarrgon <61888458+Tarrgon@users.noreply.github.com>
2025-05-29 12:31:57 -0400

    Fix renamed method.

--- src/events/handle-guild-create.ts
+++ src/events/handle-guild-create.ts
@@ -2,7 +2,7 @@ import { Database } from '../shared/Database';
 
 export async function handleGuildCreate(guild) {
   try {
-    if (!await Database.getGuildSettings(guild.id)) await Database.addGuild(guild.id);
+    if (!await Database.getGuildSettings(guild.id)) await Database.putGuild(guild.id);
   } catch (e) {
     console.error(e);
   }
