5a8ca6bb54f47130781c99a957cf49ad47b1ddf6
Tarrgon <61888458+Tarrgon@users.noreply.github.com>
2025-05-31 11:10:30 -0400

    Move context menus to their own folder

--- src/utils/refresh-commands.ts
+++ src/utils/refresh-commands.ts
@@ -13,27 +13,40 @@ export async function refreshCommands(client: Client) {
   try {
     const commands: RESTPostAPIApplicationCommandsJSONBody[] = [];
     const guildCommands: { [id: string]: RESTPostAPIApplicationCommandsJSONBody[] } = {};
-    const commandFiles = fs.readdirSync(`${ROOT_DIR}/commands`).filter(file => file.endsWith('.js') || file.endsWith('.ts'));
+    const commandFiles = {
+      commands: fs.readdirSync(`${ROOT_DIR}/commands`).filter(file => file.endsWith('.js') || file.endsWith('.ts')),
+      'context-menus': fs.readdirSync(`${ROOT_DIR}/context-menus`).filter(file => file.endsWith('.js') || file.endsWith('.ts')),
+    };
 
-    for (const file of commandFiles) {
-      // eslint-disable-next-line @typescript-eslint/no-require-imports
-      const command: Command = require(`${ROOT_DIR}/commands/${file}`).default;
-      let data: RESTPostAPIApplicationCommandsJSONBody;
-      if (typeof (command.data) == 'function') {
-        data = (await command.data(client)).toJSON();
-      } else {
-        data = command.data.toJSON();
-      }
+    for (const [folderName, files] of Object.entries(commandFiles)) {
+      for (const file of files) {
+        const p = `${ROOT_DIR}/${folderName}/${file}`;
+        // eslint-disable-next-line @typescript-eslint/no-require-imports
+        const command: Command = require(p).default;
 
-      if (!command.guilds) {
-        commands.push(data);
-      } else {
-        for (const id of command.guilds) {
-          if (!guildCommands[id]) guildCommands[id] = [];
-          guildCommands[id].push(data);
+        if (!command) {
+          console.warn(`File at ${p} has no export. Skipping registering.`);
+          continue;
+        }
+
+        let data: RESTPostAPIApplicationCommandsJSONBody;
+        if (typeof (command.data) == 'function') {
+          data = (await command.data(client)).toJSON();
+        } else {
+          data = command.data.toJSON();
+        }
+
+        if (!command.guilds) {
+          commands.push(data);
+        } else {
+          for (const id of command.guilds) {
+            if (!guildCommands[id]) guildCommands[id] = [];
+            guildCommands[id].push(data);
+          }
         }
       }
     }
+
     console.log('Started refreshing application (/) commands.');
 
     console.log('Global commands: ' + commands.length);
