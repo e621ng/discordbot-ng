403de9480eadae40ccafbb870295c5d767761426
Tarrgon <61888458+Tarrgon@users.noreply.github.com>
2025-06-04 13:57:08 -0400

    Fix threads not being in staff categories

--- src/utils/channel-utils.ts
+++ src/utils/channel-utils.ts
@@ -2,9 +2,11 @@ import { Channel, GuildBasedChannel, GuildChannel, GuildTextBasedChannel, TextBa
 import { Database } from '../shared/Database';
 
 export async function channelIsInStaffCategory(channel: GuildBasedChannel) {
-  if (!channel.guildId) return false;
+  if (!channel.guildId || !channel.parentId) return false;
 
   const staffCategories = await Database.getGuildStaffCategories(channel.guildId);
 
-  return staffCategories.includes(channel.parentId!);
+  const parentChannel = await channel.guild.channels.fetch(channel.parentId);
+
+  return parentChannel?.parentId ? staffCategories.includes(parentChannel.parentId) : staffCategories.includes(channel.parentId);
 }
