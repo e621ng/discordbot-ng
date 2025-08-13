f7b2f2854002e38203c4ebffbb5fbeda67569dfe
Tarrgon <61888458+Tarrgon@users.noreply.github.com>
2025-08-13 11:42:53 -0400

    Fix commas breaking contributor mentions

--- src/utils/github-user-utils.ts
+++ src/utils/github-user-utils.ts
@@ -1,12 +1,13 @@
 import { Database } from '../shared/Database';
 
-const mentionRegex = new RegExp('@([\\S]+)', 'gi');
+const mentionRegex = new RegExp('@([\\S]+),|@([\\S]+)', 'gi');
 
 export async function fixPings(body: string): Promise<string> {
   const mappings = await Database.getAllGithubUserMappings();
 
-  return body.replaceAll(mentionRegex, (match, username) => {
-    const mapping = mappings.find(m => m.github_username == username);
+  return body.replaceAll(mentionRegex, (match, m1, m2) => {
+    const name = m1 ?? m2;
+    const mapping = mappings.find(m => m.github_username == name);
 
     return mapping ? `<@${mapping.discord_id}>` : match;
   });
