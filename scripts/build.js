833f1462f82910f1003af76460ee9290663561ef
Tarrgon <61888458+Tarrgon@users.noreply.github.com>
2026-06-13 18:26:26 -0400

    Switch to consistent single quotes

--- scripts/build.js
+++ scripts/build.js
@@ -1,7 +1,8 @@
+// eslint-disable-next-line @typescript-eslint/no-require-imports
 const esbuild = require('esbuild');
 
 esbuild.buildSync({
-  entryPoints: ["src/**/*.ts"],
+  entryPoints: ['src/**/*.ts'],
   outdir: 'dist',
 
   platform: 'node',
